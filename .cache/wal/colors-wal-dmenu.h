efeed204c77c3c8c449254305fbe7d6bc1ea372d
comploplo <goppenheimer@umass.edu>
2020-09-18 09:29:57 -0500

    oh yeah

--- .cache/wal/colors-wal-dmenu.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#f5f5f5", "#151515" },
-	[SchemeSel] = { "#f5f5f5", "#fb9fb1" },
-	[SchemeOut] = { "#f5f5f5", "#12cfc0" },
-};
--- .cache/wal/colors-wal-tabbed.h
+++
@@ -1,6 +0,0 @@
-static const char* selbgcolor   = "#151515";
-static const char* selfgcolor   = "#f5f5f5";
-static const char* normbgcolor  = "#acc267";
-static const char* normfgcolor  = "#f5f5f5";
-static const char* urgbgcolor   = "#fb9fb1";
-static const char* urgfgcolor   = "#f5f5f5";
